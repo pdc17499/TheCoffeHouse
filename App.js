d85b1b09dc09ac777c0cd4a5a682655315f4d0ac
pdc17499 <43161538+pdc17499@users.noreply.github.com>
2021-08-12 22:59:06 +0700

    Add bottom tab 1

--- App.js
+++ App.js
@@ -1,10 +1,74 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import HomeScreen from './src/screens/HomeScreen';
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Chào bạn mới"
+      screenOptions={{
+        tabBarActiveTintColor: 'black',
+      }}>
+      <Tab.Screen
+        name="Chào bạn mới"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Trang chủ',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Shop"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Đặt hàng',
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons
+              name="search"
+              color={color}
+              size={size}></MaterialIcons>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cửa hàng"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Wishlist',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Bag"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Tích điểm',
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons
+              name="shopping-bag"
+              color={color}
+              size={size}></MaterialIcons>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Cin chào</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
